2409b1f9e97854bfdeabd2982b7d07866311b08c
PaulieScanlon <pauliescanlon@gmail.com>
2025-04-17 09:33:28 +0100

    feat: add neon database

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,10 @@
 import { useState } from 'react';
 
 const App = ({ data }) => {
-  const [count, setCount] = useState(0);
-
   return (
     <main>
-      <h1>App</h1>
-      <p>Lorem Ipsum</p>
-      <div>
-        <div>{count}</div>
-        <button onClick={() => setCount(count + 1)}>Count</button>
-      </div>
+      <h1>Neon Local</h1>
+      <p>Simple React SSR Vite Express</p>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </main>
   );
--- src/function.js
+++ src/function.js
@@ -1,6 +1,10 @@
+import 'dotenv/config';
+
+import { sql } from './db';
+
 export const getServerData = async () => {
-  const response = await fetch('https://dummyjson.com/products/1');
-  const data = await response.json();
+  const book_id = 1;
+  const data = await sql`SELECT * FROM books WHERE book_id = ${book_id}`;
 
   return data;
 };
